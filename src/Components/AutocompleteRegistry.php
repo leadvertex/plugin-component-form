443e1954b2c8602aed4419e1d2c46200c36db421
XAKEPEHOK <icq-475204@ya.ru>
2020-11-30 22:43:26 +0300

    AutocompleteRegistry

---
+++ src/Components/AutocompleteRegistry.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created for plugin-component-form
+ * Date: 30.11.2020
+ * @author Timur Kasumov (XAKEPEHOK)
+ */
+
+namespace Leadvertex\Plugin\Components\Form\Components;
+
+
+use RuntimeException;
+
+final class AutocompleteRegistry
+{
+
+    /** @var callable */
+    private static $resolver;
+
+    private function __construct() {}
+
+    public static function config(callable $resolver): void
+    {
+        self::$resolver = $resolver;
+    }
+
+    /**
+     * @param string $name
+     * @return AutocompleteInterface
+     */
+    public static function getAutocomplete(string $name): AutocompleteInterface
+    {
+        if (!isset(self::$resolver)) {
+            throw new RuntimeException('Autocomplete registry was not configured');
+        }
+
+        $resolver = self::$resolver;
+        return $resolver($name);
+    }
+
+}
